3c3bcc34034d9a43570566dfee92a4303b80e523
E-Codz <iamecodzz@proton.me>
2022-07-22 08:36:03 +0200

    make back/forward buttons work

--- windows/browser.py
+++ windows/browser.py
@@ -15,17 +15,21 @@ class Browser(Gtk.Window):
         backbtnbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         Gtk.StyleContext.add_class(backbtnbox.get_style_context(), "linked")
 
-        button = Gtk.Button()
-        button.add(
+        backbtn = Gtk.Button()
+        backbtn.add(
             Gtk.Arrow(arrow_type=Gtk.ArrowType.LEFT, shadow_type=Gtk.ShadowType.NONE)
         )
-        backbtnbox.add(button)
+        backbtn.connect("clicked", self.goBack)
+        backbtnbox.add(backbtn)
+        self.backbtn = backbtn
 
-        button = Gtk.Button()
-        button.add(
+        forwardbtn = Gtk.Button()
+        forwardbtn.add(
             Gtk.Arrow(arrow_type=Gtk.ArrowType.RIGHT, shadow_type=Gtk.ShadowType.NONE)
         )
-        backbtnbox.add(button)
+        forwardbtn.connect("clicked", self.goForward)
+        backbtnbox.add(forwardbtn)
+        self.forwardbtn = forwardbtn
 
         topbar.pack_start(backbtnbox, True, True, 0)
 
@@ -37,4 +41,24 @@ class Browser(Gtk.Window):
 
         webView = WebKit2.WebView()
         self.add(webView)
+        webView.connect("load-changed", self.load)
+        self.webView = webView
         GLib.idle_add(webView.load_uri, 'http://www.google.com')
+
+    def goBack(self, button):
+        self.webView.go_back()
+    
+    def goForward(self, button):
+        self.webView.go_forward()
+
+    def load(self, _, __):
+        if self.webView.can_go_back():
+            self.backbtn.set_sensitive(True)
+        else:
+            self.backbtn.set_sensitive(False)
+        
+        if self.webView.can_go_forward():
+            self.forwardbtn.set_sensitive(True)
+        else:
+            self.forwardbtn.set_sensitive(False)
+
